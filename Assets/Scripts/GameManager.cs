f0dc56558f5c8d45c9b81965f96a28004045f99b
jorgemedinaleigh <jorgemedinaleigh@protonmail.com>
2021-07-17 18:22:29 -0400

    Add score track with different points for different objects, penalty for click spam and particle effects

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     private float spawnRate = 1f;
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public int mouseClicks;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        UpdateScore(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            mouseClicks++;
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score = score + scoreToAdd - (mouseClicks - 1);
+        scoreText.text = "Score: " + score;
     }
 
     IEnumerator SpawnTarget()
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private GameManager gameManager;
     private float minSpeed = 12f;
-    private float maxSpeed = 20f;
+    private float maxSpeed = 16f;
     private float maxTorque = 10f;
     private float xRange = 5;
-    private float ySpawnPos = -6;
+    private float ySpawnPos = -3;
+    public ParticleSystem explosionParticle;
 
     // Start is called before the first frame update
     void Start()
     {
-        targetRb = GetComponent<Rigidbody>();
+        targetRb = GetComponent<Rigidbody>(); 
+
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque());
-        transform.position = RandomSpawnPos();    
+        transform.position = RandomSpawnPos();   
+
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>(); 
     }
 
     // Update is called once per frame
@@ -29,6 +34,28 @@ public class Target : MonoBehaviour
     private void OnMouseDown() 
     {  
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        if(gameObject.name == "Barril(Clone)")
+        {
+            gameManager.UpdateScore(10);
+            gameManager.mouseClicks = 0;
+        }
+        else if(gameObject.name == "Caja(Clone)")
+        {
+            gameManager.UpdateScore(1);
+            gameManager.mouseClicks = 0;
+        }
+        else if(gameObject.name == "CajaRandom(Clone)")
+        {
+            int randomScore = Random.Range(-5, 15);
+            gameManager.UpdateScore(randomScore);
+            gameManager.mouseClicks = 0;
+        }
+        else if(gameObject.name == "Bomba(Clone)")
+        {
+            gameManager.UpdateScore(-10);
+            gameManager.mouseClicks = 0;
+        }
     }
     private void OnTriggerEnter(Collider other) 
     {
